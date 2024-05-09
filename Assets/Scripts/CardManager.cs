ea4b55dea751ddd5840aab7d67189feef9c42a4d
WaitThere <canberkcavusoglu@hotmail.com>
2024-05-09 23:56:22 +0300

    player hand created
    player hand created and shuffling has been implemented.

---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public List<Card> ListCard;
+
+    public Stack<Card> Pool;
+
+    public Card GetCard(CardData data)
+    {
+        return ListCard.Find( x => x.CardData == data);
+    }
+
+    public void Shuffle()
+    {
+        Pool = new Stack<Card>(ListCard);
+        Pool.Shuffle();
+    }
+
+}
+
+
+
+
+
