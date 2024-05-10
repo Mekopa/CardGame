e2aec8cfbc8544bedbd135b65d1b02d49c74a9d6
Mert Cayir <mr.mekopa@gmail.com>
2024-05-10 00:34:10 -0700

    Dragger
    Dragger implemented

---
+++ Assets/Scripts/Dragger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragger : MonoBehaviour
+{
+    private Vector3 _dragOffset;
+    private Camera _cam;
+
+    private void Awake()
+    {
+        _cam = Camera.main;
+    }
+
+    void OnMouseDown()
+    {
+        _dragOffset = transform.position - GetMausePos();
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMausePos() + _dragOffset;
+    }
+
+    Vector3 GetMausePos() {
+        var mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
+        return mousePos;
+    }
+}
