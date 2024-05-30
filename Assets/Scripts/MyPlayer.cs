c7897a9ccdce793eeab45d1f9845a9461d3f7581
isabelperalta <115087059+isabelperalta@users.noreply.github.com>
2024-05-30 04:59:17 +0300

    Turn managment added

--- Assets/Scripts/MyPlayer.cs
+++ Assets/Scripts/MyPlayer.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class MyPlayer : MonoBehaviour
 {
+    public string idName;
     public List<Card> myCards;
+
+    void Start()
+    {
+        this.idName = PlayerPrefs.GetString("Username");
+    }
+
     public void TakeCard(Card card)
     {
         myCards.Add(card);
         card.transform.parent = transform;
     }
+
+    // return false if the player plays a 10
+    public bool InitTurn()
+    {
+        return true;
+        // TODO implement playing a card logic
+    }    
 }
+
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,6 +7,25 @@ public class PlayerManager : MonoBehaviour
     public CardManager CardManager;
     public List<MyPlayer> players;
 
+    // the index of the player who is playing now
+    private int playerindex;
+
+    // true while the game is going on
+    private bool isGameActive;
+    // IMPORTANT TO TURN IT FALSE WHEN SOMEBODY LOSES
+
+    void Start()
+    {
+        // starts the player with index 0
+        playerindex = 0;
+
+        // when the game starts
+        isGameActive = true;
+
+        // activates the turns loop
+        StartCoroutine(this.PlayingLoop());
+    }
+
     public void GiveCards(int amount)
     {
         foreach (var player in players)
@@ -14,6 +33,25 @@ public class PlayerManager : MonoBehaviour
             for (int i = 0; i < amount; i++)
             {
                 player.TakeCard(CardManager.Pool.Pop());
+            }
+        }
+    }
+
+    // this method is to change the turn each time a player plays
+    IEnumerator PlayingLoop()
+    {
+        while (this.isGameActive)
+        {
+            yield return new WaitForSeconds(2f);
+
+            MyPlayer currentTurn = this.players[this.playerindex];
+            currentTurn.InitTurn();
+
+            // if the card played is not a 10
+            if (!currentTurn.InitTurn())
+            {
+                // changes the turn
+                this.playerindex = (this.playerindex + 1) % this.players.Count;
             }
         }
     }
