f6865ba75ac5bf2257d000d3f8c33eaf86e83c20
Mert Cayir <mr.mekopa@gmail.com>
2024-06-06 00:19:23 -0700

    fix
    fix

--- Assets/Scripts/CardPositionManager.cs
+++ Assets/Scripts/CardPositionManager.cs
@@ -6,12 +6,16 @@ public class CardPositionMannager : MonoBehaviour
     public GameObject player2;
     public GameObject closeCard1;
     public GameObject closeCard2;
+    public GameObject openCard1;
+    public GameObject openCard2;
 
     // Define the positions where you want the objects to be placed
     private Vector3 player1Position = new Vector3((float)-4.5, 5, 0);
     private Vector3 player2Position = new Vector3((float)-4.5, -5, 0);
     private Vector3 closeCard1Position = new Vector3(-2, (float)1.2, 0);
     private Vector3 closeCard2Position = new Vector3(-2, (float)-1.2, 0);
+    private Vector3 openCard1Position = new Vector3(-2, (float)1.2, 0);
+    private Vector3 openCard2Position = new Vector3(-2, (float)-1.2, 0);
 
     void Start()
     {
@@ -22,12 +26,16 @@ public class CardPositionMannager : MonoBehaviour
         SetGlobalPosition(player2, player2Position, "Player 2");
         SetGlobalPosition(closeCard1, closeCard1Position, "Close Card 1");
         SetGlobalPosition(closeCard2, closeCard2Position, "Close Card 2");
+        SetGlobalPosition(openCard1, openCard1Position, "Open Card 1");
+        SetGlobalPosition(openCard2, openCard2Position, "Open Card 2");
 
         // Position the individual cards within each parent
         PositionCardsLocally(player1, 1.5f, "Player 1");
         PositionCardsLocally(player2, 1.5f, "Player 2");
         PositionCardsLocally(closeCard1, 1.5f, "Close Card 1");
         PositionCardsLocally(closeCard2, 1.5f, "Close Card 2");
+        PositionCardsLocally(openCard1, 1.5f, "Open Card 1");
+        PositionCardsLocally(openCard2, 1.5f, "Open Card 2");
     }
 
     void SetGlobalPosition(GameObject obj, Vector3 position, string name)
@@ -75,6 +83,12 @@ public class CardPositionMannager : MonoBehaviour
 
         Gizmos.color = Color.yellow;
         DrawGizmoForObject(closeCard2, closeCard2Position);
+
+        Gizmos.color = Color.magenta;
+        DrawGizmoForObject(openCard1, openCard1Position);
+
+        Gizmos.color = Color.white;
+        DrawGizmoForObject(openCard2, openCard2Position);
     }
 
     void DrawGizmoForObject(GameObject obj, Vector3 position)
